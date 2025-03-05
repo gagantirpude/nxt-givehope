c56e036f780a824f3a9f93ed8b12c5c4c9d84fd3
Gagan Tirpude <95214697+gagantirpude@users.noreply.github.com>
2025-03-05 21:54:37 +0530

    Add initial layout components and styles for Home, Header, and Footer

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
+        <Header />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   );
